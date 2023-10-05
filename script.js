f07b9132826a222005257b25e99e462a897e8719
ProgrammerOm <79402285+Om-Chavan@users.noreply.github.com>
2023-10-05 19:32:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let typed = new Typed(".multiple-text", {
+    strings: ["Web developer","Frontend Developer", "Web designer"
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true,
+})
